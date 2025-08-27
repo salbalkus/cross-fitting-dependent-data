037a7523ee2263c7b2b775cecca6b6e41b94a994
Salvador Balkus <salbalkus@gmail.com>
2025-08-26 21:21:25 -0400

    add renv::restore

--- scripts/run_network.R
+++ scripts/run_network.R
@@ -4,7 +4,9 @@ args = commandArgs(trailingOnly=TRUE)
 n = as.numeric(args[1])     # First argument is the sample size
 reps = as.numeric(args[2])  # Second argument is the number of replicates
 
+renv::restore(prompt = FALSE)
 renv::load(here::here())
+
 library(here)
 source(here("R", "new_network.R"))
 print(paste("Running network simulation with n =", n, "and reps =", reps))
